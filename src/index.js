147408f05affb2f13d7acdddb4a0b4e86a3a1e2b
Çağrı Evren <cagrievren93@gmail.com>
2020-10-29 00:28:31 +0300

    initializing state through constructors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props); // Reason to use super: Override React.Component's super() method inside.
+
+    this.state = { lat: null };
+  }
+
+  // React says we have to define render!
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
