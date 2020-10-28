c5633f12d52f04a797552e732735cc3773e070ff
Çağrı Evren <cagrievren93@gmail.com>
2020-10-29 00:42:19 +0300

    updating state properties

--- src/index.js
+++ src/index.js
@@ -5,17 +5,24 @@ class App extends React.Component {
   constructor(props) {
     super(props); // Reason to use super: Override React.Component's super() method inside.
 
+    // THIS IS THE ONLY TIME we do direct assignment to this.state
     this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // We called setState!!
+        this.setState({ lat: position.coords.latitude });
+
+        // We did not something like above!!!
+        // this.state.lat = position.coords.latitude
+      },
+      (err) => console.log(err)
+    );
   }
 
   // React says we have to define render!
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-
-    return <div>Latitude: </div>;
+    return <div>Latitude: { this.state.lat } </div>;
   }
 }
 
